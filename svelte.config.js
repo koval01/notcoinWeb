e93e7a154e51e8f8f0a42c0203c6d2b80954939b
Koval Yaroslav <yaroslav@koval.page>
2024-02-06 13:29:59 +0200

    Cloudflare Pages

---
+++ svelte.config.js
@@ -0,0 +1,12 @@
+import adapter from '@sveltejs/adapter-cloudflare';
+
+export default {
+	kit: {
+		adapter: adapter({
+			routes: {
+				include: ['/*'],
+				exclude: ['<all>']
+			}
+		})
+	}
+};
