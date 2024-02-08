44a3cb73b2992962da44e845418a64b6677f1a16
Koval Yaroslav <yaroslav@koval.page>
2024-02-08 16:37:04 +0200

    Update Footer styles

--- src/elements/Footer.svelte
+++ src/elements/Footer.svelte
@@ -1,9 +1,9 @@
 <footer>
-    <div class="footnote typeface-footnote margin">
-        <span>Built with 😆 by <a href="https://openbuilders.xyz" target="_blank">Open Builders</a></span>
-        <span>Reassembled by <a href="https://git.koval.page" target="_blank">Yaroslav Koval</a></span
-        >
-    </div>
+  <div class="footnote typeface-footnote margin">
+      <span>Built with 😆 by <a href="https://openbuilders.xyz" target="_blank">Open Builders</a></span>
+      <span>Reassembled by <a href="https://git.koval.page" target="_blank">Yaroslav Koval</a></span
+      >
+  </div>
 </footer>
 
 <style lang="sass">
@@ -11,13 +11,14 @@
     color: #000
     background: #ffce4e
     padding: 3px
+    font-size: 11px
     border-radius: 8px
     font-weight: 600
     text-decoration: none
 
   span::before
     content: "| "
-  
+
   .footnote
     position: relative
     text-align: center
@@ -25,15 +26,22 @@
 
   span:first-child::before
     content: ""
-  
+
   .typeface-footnote
     font-size: 13px
     font-weight: 400
     line-height: 18px
-  
+
   .margin
     margin: 20px 0
     margin-top: 32px
-  
+
+  @media (max-width: 768px)
+    span
+      display: block
+      margin-top: 5px
+    
+    span::before
+      content: ""
 
 </style>
