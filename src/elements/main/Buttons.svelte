7418b7aa114b4bbe57b3fc85802ab4c06a5f3532
Koval Yaroslav <yaroslav@koval.page>
2024-02-09 15:15:50 +0200

    Return button design to default

--- src/elements/main/Buttons.svelte
+++ src/elements/main/Buttons.svelte
@@ -17,12 +17,12 @@
   
     > .button
       border-radius: 1rem
-      background-color: #fff
+      background-color: #f90
       font-weight: 600
       padding: .75rem 1.5rem
       font-size: 1rem
       display: flex
-      color: #000
+      color: #fff
   
   @media (min-width: 768px)
     div.tapToEarn
