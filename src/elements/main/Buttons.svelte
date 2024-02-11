ec5d558278a34c1b27b6829c52772f66803e1924
Koval Yaroslav <yaroslav@koval.page>
2024-02-11 21:03:02 +0200

    Add redirect button for Battles

--- src/elements/main/Buttons.svelte
+++ src/elements/main/Buttons.svelte
@@ -1,14 +1,19 @@
 <script>
     import AnimatedButton from "../misc/AnimatedButton.svelte";
+    import CrownButton from "../misc/CrownButton.svelte";
 
-    const handleClick = () => {
+    const handleTap = () => {
         window.open("https://t.me/notcoin_bot/click", "_blank");
         document.activeElement.blur();
+    };
+    const goBattle = () => {
+        window.open("/battle", "_self");
     };
 </script>
 
 <div class="tapToEarn">
-  <AnimatedButton onClick={handleClick}>Tap-to-earn</AnimatedButton>
+  <AnimatedButton onClick={handleTap}>Tap-to-earn</AnimatedButton>
+  <CrownButton onClick={goBattle} />
 </div>
 
 <style lang="sass">
---
+++ src/elements/misc/CrownButton.svelte
@@ -0,0 +1,47 @@
+<script>
+    export let onClick;
+  
+    function handleClick() {
+      onClick();
+    }
+</script>
+
+<div class="crownButton" on:click={handleClick}>
+    <div class="crown"></div>
+</div>
+
+<style lang="sass">
+    .crownButton
+      transform: rotate(12deg)
+      cursor: pointer
+      perspective: 300px
+
+    .crown
+      background-image: url(/images/crown.webp)
+      background-size: cover
+      width: 140px
+      height: 140px
+      position: relative
+      will-change: transform
+      transform-style: preserve-3d
+    
+    .crown:hover
+      animation: shakeAnimation .5s ease 3 alternate
+    
+    @keyframes shakeAnimation
+      0%
+        transform: rotateX(0deg) rotateY(0deg) rotateZ(0deg)
+
+      25%
+        transform: rotateX(6deg) rotateY(6deg) rotateZ(6deg)
+
+      50%
+        transform: rotateX(-6deg) rotateY(-6deg) rotateZ(-6deg)
+
+      75%
+        transform: rotateX(6deg) rotateY(6deg) rotateZ(6deg)
+
+      100%
+        transform: rotateX(0deg) rotateY(0deg) rotateZ(0deg)
+    
+</style>
