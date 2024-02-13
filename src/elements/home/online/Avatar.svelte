6589e753917a7b575dc1b83be7f93bc48467e840
Koval Yaroslav <yaroslav@koval.page>
2024-02-13 21:26:16 +0200

    Update Online element Avatars

---
+++ src/elements/home/online/Avatar.svelte
@@ -0,0 +1,36 @@
+<script>
+    import { getAvatarThumb, handleImageError } from "../../../utils.js";
+
+    export let avatarSrc = undefined, onError = handleImageError;
+</script>
+
+<div class="avatar">
+    <img
+        class="avatarImg"
+        src={avatarSrc ? avatarSrc : getAvatarThumb()}
+        draggable="false"
+        alt="Avatar"
+        height="32px"
+        width="32px"
+        on:error={onError}
+    />
+</div>
+
+<style lang="sass">
+    div.avatar
+      padding: .25rem
+      border-radius: 50%
+      background-color: #000
+      margin-right: -.75rem
+  
+      > img.avatarImg
+        border-radius: 50%
+        height: 1.5rem
+        width: 1.5rem
+
+    @media (min-width: 768px)
+      div.avatar > img.avatarImg
+        height: 2rem
+        width: 2rem
+
+</style>
--- src/elements/home/online/Avatars.svelte
+++ src/elements/home/online/Avatars.svelte
@@ -1,5 +1,6 @@
 <script>
-    import { handleImageError, getAvatarThumb } from "../../../utils.js";
+    import Avatar from "./Avatar.svelte";
+    import { getAvatarThumb } from "../../../utils.js";
 
     export let users, preloadImgState;
 </script>
@@ -7,30 +8,11 @@
 <div class="avatars">
     {#if preloadImgState === void 0}
         {#each Array(3) as _, i}
-            <div class="avatar" key={i}>
-                <img
-                    class="avatarImg"
-                    src={getAvatarThumb()}
-                    draggable="false"
-                    alt="Avatar"
-                    height="32px"
-                    width="32px"
-                />
-            </div>
+            <Avatar avatarSrc={getAvatarThumb()} onError={void 0} />
         {/each}
     {:else}
         {#each users as { avatar }}
-            <div class="avatar">
-                <img
-                    class="avatarImg"
-                    src={avatar}
-                    draggable="false"
-                    on:error={handleImageError}
-                    alt="Avatar"
-                    height="32px"
-                    width="32px"
-                />
-            </div>
+            <Avatar avatarSrc={avatar} />
         {/each}
     {/if}
 </div>
@@ -40,24 +22,9 @@
       display: flex
       padding-right: .75rem
       min-width: 72px
-  
-      > div.avatar
-        padding: .25rem
-        border-radius: 50%
-        background-color: #000
-        margin-right: -.75rem
-  
-        > img.avatarImg
-          border-radius: 50%
-          height: 1.5rem
-          width: 1.5rem
 
     @media (min-width: 768px)
       div.avatars
         min-width: 96px
-
-        > div.avatar > img.avatarImg
-          height: 2rem
-          width: 2rem
 
 </style>
