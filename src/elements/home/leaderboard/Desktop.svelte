2ce0516a7dc1c504eae7e768c58089f81b49f89e
Koval Yaroslav <yaroslav@koval.page>
2024-02-13 16:15:23 +0200

    Update Leaderboard on Home for Desktop

--- src/elements/home/leaderboard/Desktop.svelte
+++ src/elements/home/leaderboard/Desktop.svelte
@@ -1,72 +1,28 @@
 <script>
+  import Hint from "./Hint.svelte";
   import { Leaderboard } from "../../misc/leaderboard";
   import { allStatUsers, allStatTeams } from "../../../store.js";
 </script>
 
-
-<!-- <div class="container">
-    <div class="thNumber">#</div>
-    <h4 class="thTitle">top miners</h4>
-</div>
-
-
-<div class="container">
-    <div class="thNumber">#</div>
-    <h4 class="thTitle">top teams</h4>
-</div> -->
-
 <div class="container">
   <div class="list">
+    <Hint>top miners</Hint>
     <Leaderboard StoreObject={$allStatUsers.leaderboard} teamsDisplay={false} />
   </div>
   <div class="list">
+    <Hint>top teams</Hint>
     <Leaderboard StoreObject={$allStatTeams.leaderboard} teamsDisplay={true} />
   </div>
 </div>
 
 <style lang="sass">
   .container
+    margin-top: 2rem
     width: 100%
     display: flex
     gap: 1rem
 
     > .list
       width: calc(50% - .5rem)
-
-  .container0
-    background-color: #ebebf520
-    border-radius: .75rem
-    width: 100%
-    display: grid
-    grid-template-columns: auto 1fr auto
-    row-gap: .2rem
-    column-gap: 1rem
-    align-content: start
-    z-index: 20
-    -webkit-backdrop-filter: blur(5px)
-    backdrop-filter: blur(5px)
-    line-height: normal
-    padding-bottom: 12px
-
-    > .thNumber
-      display: flex
-      align-items: center
-      justify-content: center
-      margin-top: 16px
-      margin-bottom: 3px
-      padding-left: 1rem
-      color: #ebebf599
-      font-weight: 600
-    
-    > .thTitle
-      color: #ebebf599
-      font-weight: 600
-      letter-spacing: 4px
-      font-size: 12px
-      display: flex
-      margin-top: 16px
-      margin-bottom: 3px
-      align-items: center
-      text-transform: uppercase
 
 </style>
---
+++ src/elements/home/leaderboard/Hint.svelte
@@ -0,0 +1,46 @@
+<div class="hint">
+    <div class="thNumber">#</div>
+    <h4 class="thTitle">
+        <slot></slot>
+    </h4>
+</div>
+
+<style lang="sass">
+  .hint
+    background-color: rgba(255, 255, 255, .12)
+    border-radius: .75rem
+    width: 100%
+    display: grid
+    grid-template-columns: auto 1fr auto
+    row-gap: .2rem
+    column-gap: 1rem
+    align-content: start
+    z-index: 20
+    -webkit-backdrop-filter: blur(5px)
+    backdrop-filter: blur(5px)
+    line-height: normal
+    padding-bottom: 12px
+    margin-bottom: .8rem
+
+    > .thNumber
+      display: flex
+      align-items: center
+      justify-content: center
+      margin-top: 16px
+      margin-bottom: 3px
+      padding-left: 1rem
+      color: #ebebf599
+      font-weight: 600
+    
+    > .thTitle
+      color: #ebebf599
+      font-weight: 600
+      letter-spacing: 4px
+      font-size: 12px
+      display: flex
+      margin-top: 16px
+      margin-bottom: 3px
+      align-items: center
+      text-transform: uppercase
+      
+</style>
