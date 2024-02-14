08870cf18cb275bd968676c978058675c17c29bd
Koval Yaroslav <yaroslav@koval.page>
2024-02-14 16:01:34 +0200

    Add Maintenance element for Leaderboard

---
+++ src/elements/misc/leaderboard/elements/Maintenance.svelte
@@ -0,0 +1,26 @@
+<div class="placeholder">
+    <div class="image">🚧</div>
+    <div class="text">
+        under maintenance
+    </div>
+</div>
+
+<style lang="sass">
+    .placeholder
+      text-align: center
+      padding: 48px 0
+      margin: 0 auto
+
+      >
+        .image
+          font-size: 70px
+          line-height: 70px
+          margin-bottom: 16px
+
+        .text
+          font-size: 15px
+          font-weight: 400
+          line-height: 20px
+          color: #ebebf599
+
+</style>
