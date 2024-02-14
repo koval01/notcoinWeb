4ebd722ccfe61ccf6f7f955382b73980f4166a9b
Koval Yaroslav <yaroslav@koval.page>
2024-02-14 12:29:18 +0200

    Update Stars animation

--- src/elements/misc/Stars.svelte
+++ src/elements/misc/Stars.svelte
@@ -47,7 +47,9 @@
     
     *
       z-index: 0
-      animation: star 4s cubic-bezier(.88, 1.25, 0, 1.2) infinite
+      filter: none
+      transition: filter 3s
+      animation: star 8s cubic-bezier(.88, 1.25, 0, 1.2) infinite
   
   .star
     position: relative
@@ -71,11 +73,14 @@
   @keyframes star
     0%
       opacity: .8
+      filter: drop-shadow(0 0 13px rgba(255, 255, 255, 1))
 
     50%
       opacity: .5
+      filter: drop-shadow(0 0 18px rgba(255, 255, 255, .4))
 
     100%
       opacity: .8
+      filter: drop-shadow(0 0 13px rgba(255, 255, 255, 1))
   
 </style>
