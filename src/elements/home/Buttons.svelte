7b994564cfe2cf83e314d1c6eb4315383604be79
Koval Yaroslav <yaroslav@koval.page>
2024-03-13 22:17:31 +0200

    Revert buttons container on Home

--- src/elements/home/Buttons.svelte
+++ src/elements/home/Buttons.svelte
@@ -1,59 +1,27 @@
 <script>
-    import CoinButton from "../misc/CoinButton.svelte";
-    import CrownButton from "../misc/CrownButton.svelte";
+    import NotButton from "../misc/NotButton.svelte";
 
     const handleTap = () => {
         window.open("https://t.me/notcoin_bot/click", "_blank");
         document.activeElement.blur();
     };
-    const goBattle = () => {
-        window.open("/battle", "_self");
-    };
 </script>
 
-<div class="headButtons">
-  <div class="box">
-    <CoinButton onClick={handleTap} />
-  </div>
-  <div class="box">
-    <CrownButton onClick={goBattle} />
-    <div class="battle-text"></div>
-  </div>
+<div class="headButton">
+  <NotButton onClick={handleTap}>Tap-to-earn</NotButton>
 </div>
 
 <style lang="sass">
-  .battle-text
-    display: none
-
-  .headButtons
+  .headButton
     margin-bottom: 3.5rem
     display: flex
     justify-content: center
     align-items: center
     gap: 5rem
-
-  .box
-    width: 80px
-    height: 80px
   
   @media (min-width: 768px)
-    .battle-text
-      display: block
-      background-image: url(/images/battle-text.webp)
-      background-size: cover
-      position: relative
-      left: 4rem
-      bottom: 2rem
-      width: 8rem
-      height: 3rem
-      transform: rotate(355deg)
-
-    .headButtons
+    .headButton
       margin-bottom: 8rem
       gap: 8rem
-
-    .box
-      width: 140px
-      height: 140px
 
 </style>
